1e10a9f1f2c866e306108fb85014d8c9de44334d
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-03-23 19:11:57 +0200

    my first project version

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+
+        String name ="David";
+        int age = 42;
+        double score =15.9;
+        char group = 'Z';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(group);
+
+        // Ex. new
+
+        int a = 12; // days
+        int b = 24; // hours per day
+        int c = (a * b); // hours per 12 days
+        int d = 60; // minutes
+
+        System.out.println("In 12 days are " + c +" hours ");
+        System.out.println("In " + c + " hours are " + c*d + " minutes ");
+        System.out.println("And in " + c*d + " minutes are " + c*d*d + " minutes ");
+
+
+
+
+
+
+    }
+}
